6356bd5f6deba3baa4c4dd86f369d937c7b865ca
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-02-29 16:42:03 +0000

    Completed get all passenger details method.

--- start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,7 +1,9 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import com.example.airline_api.services.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +19,7 @@ public class PassengerController {
     // Display details of all passengers
     @GetMapping
     public ResponseEntity<List<Passenger>> getAllPassengers(){
-        return null;
+        return new ResponseEntity<>(passengerService.getAllPassengers(), HttpStatus.OK);
     }
 
     // Display specific passenger details
--- start_point/src/main/java/com/example/airline_api/services/PassengerService.java
+++ start_point/src/main/java/com/example/airline_api/services/PassengerService.java
@@ -1,4 +1,19 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Passenger;
+import com.example.airline_api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PassengerService {
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public List<Passenger> getAllPassengers(){
+        return passengerRepository.findAll();
+    }
 }
