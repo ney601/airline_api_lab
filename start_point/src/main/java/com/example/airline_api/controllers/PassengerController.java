c0b9c7f3eea0ce25faf526fcd99a544d7532a6d0
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-02-29 16:31:47 +0000

    Created service classes.

--- start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,6 +1,7 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/passengers")
 public class PassengerController {
+
+    @Autowired
+    PassengerService passengerService;
 
     // Display details of all passengers
     @GetMapping
